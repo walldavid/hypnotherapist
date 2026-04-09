a6975f0fe1be67db1208ec785ab478b918270228
David Wall <walldavid@gmail.com>
2026-04-09 01:09:04 +0100

    fix for sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,8 +8,8 @@ import { schemaTypes } from './sanity/schemas'
 export default defineConfig({
   name: 'hypnotherapy-site',
   title: 'Hypnotherapy.ie',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: '2lhjnuav',
+  dataset: 'production',
   basePath: '/studio',
   plugins: [structureTool(), visionTool()],
   schema: {
--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'next-sanity'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'placeholder'
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+const projectId = '2lhjnuav'
+const dataset = 'production'
 
 export const client = createClient({
   projectId,
